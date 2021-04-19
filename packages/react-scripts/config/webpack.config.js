7084602b1187575e085e9e4549467ee92c02fd79
Joey Cozza <joeycozza@gmail.com>
2021-04-19 15:13:06 -0600

    adding offline.html as an additionalManifestEntries in InjectManifest plugin

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -779,6 +779,7 @@ module.exports = function (webpackEnv) {
           swSrc,
           dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
           exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+          additionalManifestEntries: [{url: 'offline.html', revision: null}],
           // Bump up the default maximum size (2mb) that's precached,
           // to make lazy-loading failure scenarios less likely.
           // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
